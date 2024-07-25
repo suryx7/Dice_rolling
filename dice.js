90c8322e4b109b37d2ae6d1df09f0a7fe8294310
suryx7 <surendrapuri784995@gmail.com>
2024-07-25 21:50:01 +0530

    first commit

---
+++ dice.js
@@ -0,0 +1,15 @@
+function rolldice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+    for(i=0; i<numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<image src = "dice_images/dice_${value}.png" alt = "Dice : ${value}">`);
+
+    }
+    diceResult.textContent = `Dice : ${values.join(", ")}`; 
+    diceImages.innerHTML = images.join('');
+}
